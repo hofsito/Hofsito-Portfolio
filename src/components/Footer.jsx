9ee9e9a3ec39397e5421982f84ddbefed51ad998
hofsito <hofkary208@gmail.com>
2025-04-28 21:25:49 -0500

    footercorregido

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,12 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 
 const Footer = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation(); // También obtenemos i18n
   const year = new Date().getFullYear(); 
+
+  if (!i18n.isInitialized) {
+    return null; // Esperamos a que i18next esté listo
+  }
 
   return (
     <footer className="absolute bottom-0 left-0 w-full py-4 text-center text-white bg-transparent">
